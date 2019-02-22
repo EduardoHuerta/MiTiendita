fe68dbdccd7f9f0c2f50fc84fdc2ba77b1d4c66d
oosancheza <oosancheza@concredito.com.mx>
2019-02-21 22:39:34 -0700

    [MOD] se agrego logica en los comportamientos y venta

--- src/main/java/com/mcc/MiTiendita/framework/Sale.java
+++ src/main/java/com/mcc/MiTiendita/framework/Sale.java
@@ -1,18 +1,30 @@
 package com.mcc.MiTiendita.framework;
 
-import com.mcc.MiTiendita.framework.behaviors.discountBehaviors.DiscountDayBehavior;
 import com.mcc.MiTiendita.app.domain.entities.Product;
+import com.mcc.MiTiendita.framework.behaviors.discountBehaviors.DiscountDayBehavior;
 
 import java.util.List;
 
 public abstract class Sale {
 
+  public final Double IVA = 0.16;
   protected DiscountDayBehavior discountDayBehavior;
 
   public Sale() {}
 
   public Double sell(List<Product> products) {
-    return 0.0;
+
+    Double total = 0.0;
+
+    if (products != null && !products.isEmpty()) {
+      total = discountDayBehavior.applyDiscount(products);
+
+      if (total != null && total > 0) {
+        total += (total * 0.16);
+      }
+    }
+
+    return total;
   }
 
   public void setDiscountDayBehavior(DiscountDayBehavior discountDayBehavior) {
--- src/main/java/com/mcc/MiTiendita/framework/behaviors/discountBehaviors/OlderAdultDiscountDay.java
+++ src/main/java/com/mcc/MiTiendita/framework/behaviors/discountBehaviors/OlderAdultDiscountDay.java
@@ -1,12 +1,23 @@
 package com.mcc.MiTiendita.framework.behaviors.discountBehaviors;
 
+import com.mcc.MiTiendita.app.domain.ProductCategory;
 import com.mcc.MiTiendita.app.domain.entities.Product;
 
 import java.util.List;
 
 public class OlderAdultDiscountDay implements DiscountDayBehavior {
+
+  // 10 percent
+  private final Double OLDERADULT_DISCOUNT = 0.95;
+
   @Override
   public Double applyDiscount(List<Product> products) {
-    return null;
+    Double total = 0.0;
+    if (products != null && !products.isEmpty()) {
+      for (Product item : products) {
+        total += (item.getPrice() * item.getQuantity()) * OLDERADULT_DISCOUNT;
+      }
+    }
+    return total;
   }
 }
